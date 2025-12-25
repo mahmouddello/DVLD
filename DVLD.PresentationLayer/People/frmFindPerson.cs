a971ae0fa670c1941d929a786424f6054eaba9b5
mahmouddello <102472934+mahmouddello@users.noreply.github.com>
2025-12-25 19:33:20 +0300

    create find person form, fix bugs in person card control

---
+++ DVLD.PresentationLayer/People/frmFindPerson.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.PresentationLayer.People
+{
+    public partial class frmFindPerson : Form
+    {
+        // Declare a delegate
+        public delegate void DataBackEventHandler(object sender, int PersonID);
+
+        // Declare an event using the delegate
+        public event DataBackEventHandler DataBack;
+
+        public frmFindPerson()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            // Trigger the event to send databack to the caller form
+            DataBack?.Invoke(this, ctrlPersonCardWithFilter1.PersonID);
+        }
+    }
+}
